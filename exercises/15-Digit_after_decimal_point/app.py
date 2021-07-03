9f559c53db5d76d3fab9face9f0fd52280d04192
fabianchs <fabichasola@hotmail.com>
2021-07-03 01:50:56 +0000

    done until 17

--- exercises/15-Digit_after_decimal_point/app.py
+++ exercises/15-Digit_after_decimal_point/app.py
@@ -1,8 +1,10 @@
 #Complete the function to return the first digit to the right of the decimal point.
 def first_digit(num):
-  return None
+    solved_num= int((num*100//10))
+    str_num= str(solved_num)
+    return int(str_num[-1])
 
 
 
 #Invoke the function with a positive real number. ex. 34.33
-print(first_digit())
+print(first_digit(6.24))
