537cbc7028a6c3f258565996d3e4ae9fe1a09129
fabianchs <fabichasola@hotmail.com>
2021-07-06 15:47:27 +0000

    done until 20

--- exercises/20-Digital_clock/app.py
+++ exercises/20-Digital_clock/app.py
@@ -1,6 +1,9 @@
 #Complete the function to return how many hrs and min are displayed on the 24th digital clock.
 def digital_clock(n):
-  return None
+    #cantidad de horas= n//60
+    #cantidad de minutos (n//60)*60-n
+
+    return (n//60,(n-((n//60)*(60))))
 
 #Invoke the function with any interger (seconds after midnight)
-print(digital_clock())
+print(digital_clock(150))
