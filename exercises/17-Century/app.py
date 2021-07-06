ddfe9c5cff8ae57732f5deb7f3e3ac0ab38cb3d0
fabianchs <fabichasola@hotmail.com>
2021-07-06 15:24:22 +0000

    done until 19

--- exercises/17-Century/app.py
+++ exercises/17-Century/app.py
@@ -1,10 +1,12 @@
-#Complete the function to return the respective number of the century
-#HINT: You may need to import the math module.
-
+# Complete the function to return the respective number of the century
+# HINT: You may need to import the math module.
+import math
 def century(year):
-  return None
+    return (year) // 100 + 1  
 
 
 
-#Invoke the function with any given year. 
-print(century())
+
+
+# Invoke the function with any given year. 
+print(century(2017))
--- exercises/19-Day_of_week/app.py
+++ exercises/19-Day_of_week/app.py
@@ -1,9 +1,13 @@
 #Complete the function to return the number of day of the week for k'th day of year. 
 def day_of_week(k):
-  
-  return None
+  array=[4,5,6,0,1,2,3]
+  #cuantas veces cabe ese día en semanas
+  #k-núm de semana * 7
+  week_position= k-(k//7)*7-1
+
+  return (array[week_position])
 
 
 
 #Invoke function day_of_week with an interger between 0 and 6 (number for days of week)
-print(day_of_week())
+print(day_of_week(1))
